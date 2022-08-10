031a27772d0eb1700b2d668e3e051add5fbc283a
Kevin-MartinezP <kevin.pineda.martinez@gmail.com>
2022-08-09 19:23:02 -0600

    Optimizando el codigo para mostrar los datos en tiempo real

--- js/main.js
+++ js/main.js
@@ -1,58 +1,68 @@
-/*Variables*/
-const numeroTarjetaInput = document.querySelector(".numero-tarjeta-input");
-const tarjetaEnfrenteNumero = document.querySelector(
-    ".tarjeta-enfrente-numero"
-);
+/*Variable*/
+const contenedorFormulario = document.querySelector(".contenedor-formulario");
+const inputs = document.querySelectorAll(".contenedor-formulario input");
 
-const nombreTarjetaInput = document.querySelector(".nombre-tarjeta-input");
-const tarjetaEnfrenteNombre = document.querySelector(
-    ".tarjeta-enfrente-nombre"
-);
+/*Eventos */
+contenedorFormulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
 
-const fechaMmInput = document.querySelector(".fecha-mm-input");
-const fechaYyInput = document.querySelector(".fecha-yy-input");
-const tarjetaEnfrenteFecha = document.querySelector(".tarjeta-enfrente-fecha");
+/*Objetos */
+const expresiones = {
+    numero: /^[0-9]$/,
+};
 
-const cvcInput = document.querySelector(".cvc-input");
-const tarjetaTraseraCvc = document.querySelector(".tarjeta-trasera-cvc");
+/*Funciones*/
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            if (e.target.value == "") {
+                document.querySelector(".tarjeta-enfrente-nombre").innerHTML =
+                    "Kevin Armando Pineda";
+            } else {
+                document.querySelector(".tarjeta-enfrente-nombre").innerHTML =
+                    e.target.value;
+            }
+            break;
+        case "numero":
+            if (e.target.value == "") {
+                document.querySelector(".tarjeta-enfrente-numero").innerHTML =
+                    "0000 0000 0000 0000";
+            } else {
+                document.querySelector(".tarjeta-enfrente-numero").innerHTML =
+                    e.target.value;
+            }
+            break;
+        case "fecha":
+            const fechaMmInput = document.querySelector(".fecha-mm-input");
+            const fechaYyInput = document.querySelector(".fecha-yy-input");
+            const tarjetaEnfrenteFecha = document.querySelector(
+                ".tarjeta-enfrente-fecha"
+            );
 
-nombreTarjetaInput.addEventListener("keyup", enviarNombreTarjeta);
-numeroTarjetaInput.addEventListener("keyup", enviarNumeroTarjeta);
-fechaMmInput.addEventListener("keyup", enviarFechaMM);
-fechaYyInput.addEventListener("keyup", enviarFechaMM);
-cvcInput.addEventListener("keyup", enviarCvc);
-
-function enviarNombreTarjeta() {
-    tarjetaEnfrenteNombre.innerHTML = nombreTarjetaInput.value;
-
-    if (nombreTarjetaInput.value == "") {
-        tarjetaEnfrenteNombre.innerHTML = "Kevin Armando Pineda";
+            if (fechaMmInput.value == "" && !fechaYyInput.value == "") {
+                tarjetaEnfrenteFecha.innerHTML = "00" + " / " + fechaYyInput.value;
+            } else if (fechaYyInput.value == "" && !fechaMmInput.value == "") {
+                tarjetaEnfrenteFecha.innerHTML = fechaMmInput.value + " / " + "00";
+            } else if (fechaMmInput.value == "" && fechaYyInput.value == "") {
+                tarjetaEnfrenteFecha.innerHTML = "00" + " / " + "00";
+            } else {
+                tarjetaEnfrenteFecha.innerHTML =
+                    fechaMmInput.value + " / " + fechaYyInput.value;
+            }
+            break;
+        case "cvc":
+            if (e.target.value == "") {
+                document.querySelector(".tarjeta-trasera-cvc").innerHTML = "000";
+            } else {
+                document.querySelector(".tarjeta-trasera-cvc").innerHTML =
+                    e.target.value;
+            }
+            break;
     }
-}
-function enviarNumeroTarjeta() {
-    tarjetaEnfrenteNumero.innerHTML = numeroTarjetaInput.value;
+};
 
-    if (numeroTarjetaInput.value == "") {
-        tarjetaEnfrenteNumero.innerHTML = "0000 0000 0000 0000";
-    }
-}
-function enviarFechaMM() {
-    tarjetaEnfrenteFecha.innerHTML =
-        fechaMmInput.value + " / " + fechaYyInput.value;
-
-        if (fechaMmInput.value == "" && !fechaYyInput.value == "") {
-            tarjetaEnfrenteFecha.innerHTML = "00" + " / " + fechaYyInput.value;
-        }else if (fechaYyInput.value == "" && !fechaMmInput.value == "") {
-            tarjetaEnfrenteFecha.innerHTML = fechaMmInput.value + " / " + "00";
-        }else if (fechaMmInput.value == "" && fechaYyInput.value == "") {
-            tarjetaEnfrenteFecha.innerHTML = "00" + " / " + "00";
-        }
-}
-
-function enviarCvc() {
-    tarjetaTraseraCvc.innerHTML = cvcInput.value;
-
-    if (cvcInput.value == "") {
-        tarjetaTraseraCvc.innerHTML = "000";
-    }
-}
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+});
